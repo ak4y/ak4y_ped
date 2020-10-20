e1aace7896368673369fdb1d2697b290d1fb6664
Ugur <50309526+ak4y@users.noreply.github.com>
2020-10-20 12:55:14 +0300

    Add files via upload

---
+++ ak4y_ped_essentialmode/client.lua
@@ -0,0 +1,55 @@
+ESX = nil
+local cd = true
+
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+end)
+
+AddEventHandler('playerSpawned', function(spawn)
+	TriggerServerEvent("ak4y:checkPed")
+	while cd do
+		Citizen.Wait(2*60000)
+		cd=false
+	end
+end)
+
+RegisterNetEvent("ak4y:loadPed")
+AddEventHandler("ak4y:loadPed", function(pedModel)
+    if pedModel ~= nil then
+        local mhash = GetHashKey(pedModel)
+			while not HasModelLoaded(mhash) do
+				RequestModel(mhash)
+				Citizen.Wait(10)
+			end
+		Citizen.Wait(200)
+		SetPlayerModel(PlayerId(),mhash)
+		SetPedMaxHealth(PlayerPedId(),200)
+		SetEntityHealth(PlayerPedId(),200)
+		SetPedDefaultComponentVariation(PlayerPedId())
+    end
+end)
+
+RegisterNetEvent("ak4y:loadDefault")
+AddEventHandler("ak4y:loadDefault", function()
+	TriggerEvent('esx_skin:karakterYukle')
+end)
+
+RegisterNetEvent("ak4y:setNewPed")
+AddEventHandler("ak4y:setNewPed", function(model)
+	TriggerServerEvent("ak4y:savePed", model)
+	Citizen.Wait(200)
+	TriggerServerEvent("ak4y:checkPed")
+end)
+
+RegisterCommand("pedfix", function()
+	if cd then
+		ESX.ShowNotification("Bu kodu yalnızca oyuna girdiğin ilk 2 dakika kullanabilirsin.")
+	else
+		TriggerServerEvent("ak4y:checkPed")
+	end
+end)
+
+
---
+++ ak4y_ped_essentialmode/server.lua
@@ -0,0 +1,39 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+TriggerEvent('es:addGroupCommand', 'ped', 'admin', function(source, args, user)
+	if args[1] and args[2] ~= nil then
+		if ESX.GetPlayerFromId(args[1]) ~= nil then
+			local xPlayer = ESX.GetPlayerFromId(args[1])
+			if args[2] == "sil" then
+				local Identifier = xPlayer.getIdentifier()
+				MySQL.Async.fetchAll("UPDATE users SET ped = @ped WHERE identifier = @identifier",{['@identifier'] = Identifier,	['@ped'] = nil}) 
+				Citizen.Wait(100)
+				TriggerClientEvent("ak4y:pedSil", xPlayer.source)
+			else
+				TriggerClientEvent("ak4y:yeniPed", xPlayer.source, args[2])
+			end
+		end		
+	end
+end, function(source, args, user)
+	TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
+end, { help = "'Ped Adı' ya da 'sil' (kisinin pedini siler)", params = {{ name = 'id' }} })
+
+RegisterServerEvent('ak4y:savePed')
+AddEventHandler('ak4y:savePed', function(model)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local Identifier = xPlayer.getIdentifier()
+	MySQL.Async.fetchAll("UPDATE users SET ped = @ped WHERE identifier = @identifier",{['@identifier'] = Identifier,	['@ped'] = tostring(model)}) 
+end)
+
+RegisterServerEvent('ak4y:checkPed')
+AddEventHandler('ak4y:checkPed', function()
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local Identifier = xPlayer.getIdentifier()
+	MySQL.Async.fetchAll("SELECT ped FROM users WHERE identifier = @identifier", { ["@identifier"] = Identifier }, function(result)
+		if result[1].ped == "" or result[1].ped == nil then	else
+			TriggerClientEvent("ak4y:pedYukle", xPlayer.source, result[1].ped)
+		end
+    end)
+end)
